4c4c88d6417183c3fb7f857d1a5e82543da10758
myeongyolee <myeongyolee@gmail.com>
2019-05-15 14:49:40 +0900

    commit and push

--- HelloGit/src/com/kh/git/HelloGit.java
+++ HelloGit/src/com/kh/git/HelloGit.java
@@ -6,6 +6,8 @@ public class HelloGit {
 		System.out.println("안녕, Git!");
 		
 		System.out.println("gitignore.io에 eclipse, windows, java를 입력해서 gitignore파일에 붙여 넣는다.");
+		
+		System.out.println("commit and Push");
 	}
 
 }
