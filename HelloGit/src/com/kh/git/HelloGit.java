5390441bed61564807d0ac2bee574b3351652341
myeongyolee <myeongyolee@gmail.com>
2019-05-15 14:47:14 +0900

    gitignore파일 설정
    gitignore.io페이지에서 가져오는 방법

--- HelloGit/src/com/kh/git/HelloGit.java
+++ HelloGit/src/com/kh/git/HelloGit.java
@@ -4,6 +4,8 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("안녕, Git!");
+		
+		System.out.println("gitignore.io에 eclipse, windows, java를 입력해서 gitignore파일에 붙여 넣는다.");
 	}
 
 }
